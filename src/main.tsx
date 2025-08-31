f99e87d4c8c2a794d0fc3cd4fac72cbeaed2b73d
L <lmwoo0327@naver.com>
2025-09-01 01:50:22 +0900

    feat: BrowserRouter 관리 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
 
 if (import.meta.env.DEV) {
   const { worker } = await import('./mocks/browser')
   await worker.start({ onUnhandledRequest: 'bypass' })
 }
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
