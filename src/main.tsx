ecd990cf9473985014b20eb8634e5e7b55d6204f
L <lmwoo0327@naver.com>
2025-08-31 23:24:32 +0900

    chore: MSW 초기 설정 추가
    - mockServiceWorker.js 파일 등록
    - browser.ts 생성 및 워커 핸들러 초기화
    - main.tsx에서 개발 환경에서만 MSW 워커 시행되도록 구성

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react'
-
 import { createRoot } from 'react-dom/client'
-
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser')
+  await worker.start({ onUnhandledRequest: 'bypass' })
+}
+createRoot(document.getElementById('root')!).render(<App />)
