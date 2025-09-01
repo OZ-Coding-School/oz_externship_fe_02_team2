7cf4788b46703dda263dbb1eececb8fd1ba9a727
Makee <ina0312@naver.com>
2025-09-01 17:11:05 +0900

    feat: Button 컴포넌트에 props 반영(#20)

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,10 +1,16 @@
-export default function Button() {
+import type { ButtonProps } from './Button.types'
+
+export default function Button(props: ButtonProps) {
+  const {
+    btnStyle = 'primary',
+    btnSize = 'medium',
+    btnIcon,
+    btnText,
+    onClick,
+  } = props
+
   return (
-    <button
-      type="button"
-      onClick={() => console.log('button clicked!')}
-      className=""
-    >
+    <button type="button" onClick={onClick} className="">
       {btnIcon && <span>{btnIcon}</span>}
       {btnText && btnText}
     </button>
