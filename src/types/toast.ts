44771c3403e4ff37a1d2177af20a8d22441e5a8a
Makee <ina0312@naver.com>
2025-09-03 11:01:59 +0900

    feat: ToastStore interface 추가(#35)

--- src/types/toast.ts
+++ src/types/toast.ts
@@ -1,8 +1,14 @@
 import type { ToastStyle } from '@/components/ui/Toast/Toast.types'
 
 export interface ToastProps {
-  id: number | string
+  id: number
   type: ToastStyle
   title: string
   content: string
+}
+
+export interface ToastStore {
+  toasts: ToastProps[]
+  addToast: (newToast: ToastProps) => void
+  removeToast: (id: number) => void
 }
