8dc54e84d743af97e06869989aec4ccf63331fcf
Makee <ina0312@naver.com>
2025-09-03 13:10:12 +0900

    feat: Toast 컴포넌트 스타일링 및 icon alt text 설정(#35)

--- src/components/ui/Toast/Toast.tsx
+++ src/components/ui/Toast/Toast.tsx
@@ -1,10 +1,12 @@
 import type { ToastProps } from '@/types'
 import {
+  TOAST_COMMON_STYLES,
   TOAST_DEFAULT_CONTENT,
   TOAST_DEFAULT_TITLE,
   TOAST_ICON,
   TOAST_STYLES,
 } from './Toast.styles'
+import { cn } from '@/lib'
 
 export function Toast(props: ToastProps) {
   const {
@@ -13,14 +15,15 @@ export function Toast(props: ToastProps) {
     content = TOAST_DEFAULT_CONTENT[type],
   } = props
   const icon = TOAST_ICON[type]
+  const iconAlt = `${TOAST_DEFAULT_TITLE[type]} 아이콘`
   const styles = TOAST_STYLES[type]
 
   return (
-    <div>
-      <img src={icon} alt={`${TOAST_DEFAULT_TITLE} 아이콘`} />
+    <div className={cn(TOAST_COMMON_STYLES, styles.border, styles.bg)}>
+      <img src={icon} alt={iconAlt} />
       <div>
-        <h6>{title}</h6>
-        <p>{content}</p>
+        <h6 className={styles.title}>{title}</h6>
+        <p className={styles.content}>{content}</p>
       </div>
     </div>
   )
