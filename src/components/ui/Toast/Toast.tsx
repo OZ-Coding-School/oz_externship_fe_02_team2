d20d7c5e7030be92eaa9d4791d74c38ba2d5f148
Makee <ina0312@naver.com>
2025-09-03 13:02:01 +0900

    feat: Toast 컴포넌트 틀 잡기(#35)

--- src/components/ui/Toast/Toast.tsx
+++ src/components/ui/Toast/Toast.tsx
@@ -8,11 +8,20 @@ import {
 
 export function Toast(props: ToastProps) {
   const {
-    id,
     type,
     title = TOAST_DEFAULT_TITLE[type],
     content = TOAST_DEFAULT_CONTENT[type],
   } = props
   const icon = TOAST_ICON[type]
   const styles = TOAST_STYLES[type]
+
+  return (
+    <div>
+      <img src={icon} alt={`${TOAST_DEFAULT_TITLE} 아이콘`} />
+      <div>
+        <h6>{title}</h6>
+        <p>{content}</p>
+      </div>
+    </div>
+  )
 }
