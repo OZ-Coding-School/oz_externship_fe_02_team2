b0217996aa2cb489df03c7f98635c65e5a625867
Makee <ina0312@naver.com>
2025-09-05 16:34:08 +0900

    feat: Sidebar 컴포넌트 생성(#44)

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@components/ui/Button'
+import Burger from '@assets/icons/hamburger.svg'
+
+export default function Sidebar() {
+  return (
+    <section>
+      <header>
+        <h4 className="font-bold">관리자 패널</h4>
+        <Button
+          btnSize="small"
+          btnIcon={<img src={Burger} alt="메인 메뉴 토글" />}
+          className="bg-transparent hover:bg-gray-100 active:bg-gray-200"
+          iconOnly
+        />
+      </header>
+      <nav>아코디언 메뉴</nav>
+    </section>
+  )
+}
