9ee427e3c007d2393f7650b685054501df3a4eb0
Makee <ina0312@naver.com>
2025-09-15 14:47:38 +0900

    feat: TestRoot 페이지에 기능 적용 및 디자인 수정(#46)

--- src/pages/test/TestRoot.tsx
+++ src/pages/test/TestRoot.tsx
@@ -1,21 +1,38 @@
 import { Button } from '@/components/ui/Button'
-import { TEST_PATHS } from '@/routes/constants'
-import { useNavigate } from 'react-router'
+import { PATHS } from '@/routes/constants'
+import { Outlet, useLocation, useNavigate } from 'react-router'
 
 export default function TestRoot() {
   const navigate = useNavigate()
+  const { pathname } = useLocation()
+  const isRoot = pathname === '/'
+
   return (
-    <main>
-      <Button
-        onClick={() => navigate(TEST_PATHS.APP)}
-        btnStyle="secondary"
-        btnText="웹 페이지로"
-      />
-      <Button
-        onClick={() => navigate(TEST_PATHS.TEST)}
-        btnStyle="primary"
-        btnText="테스트 페이지로"
-      />
-    </main>
+    <div>
+      <div className="min-h-dvh">
+        {isRoot ? (
+          <div className="grid min-h-dvh place-items-center p-6">
+            <div className="-mt-20 w-full max-w-md space-y-4 text-center">
+              <h1 className="mb-8">이동할 페이지를 선택해 주세요.</h1>
+              <div className="flex gap-3">
+                <Button
+                  onClick={() => navigate(`/${PATHS.APP}`)}
+                  btnText="웹 페이지로"
+                  className="flex-1"
+                />
+                <Button
+                  onClick={() => navigate(`/${PATHS.TEST}`)}
+                  btnText="테스트 페이지로"
+                  btnStyle="secondary"
+                  className="flex-1"
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <Outlet />
+        )}
+      </div>
+    </div>
   )
 }
