36a14e276af435242d20640ed719b5374500f19d
Makee <ina0312@naver.com>
2025-09-15 13:44:55 +0900

    feat: PAGE_TITLE 상수 추가, 페이지별 제목 매핑(#46)

--- src/routes/constants.ts
+++ src/routes/constants.ts
@@ -1,4 +1,4 @@
-export const TEST_PATHS = {
+export const PATHS = {
   TEST: '/test-hub',
   APP: '/app',
 
@@ -14,4 +14,17 @@ export const TEST_PATHS = {
 
   POST: '/post',
   APPLICATION: '/application',
+}
+
+export const PAGE_TITLE = {
+  USER: '회원 관리',
+  WITHDRAWAL: '회원 탈퇴 관리',
+  DASHBOARD: '대시보드',
+
+  LECTURE: '강의 관리',
+  STUDYGROUP: '스터디 그룹 관리',
+  REVIEW: '리뷰 관리',
+
+  POST: '스터디 구인 공고 관리',
+  APPLICATION: '지원 내역 관리',
 }
