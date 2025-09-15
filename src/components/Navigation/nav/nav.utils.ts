67def91b30df354c64927c57d9a2b5db3e0b860c
Makee <ina0312@naver.com>
2025-09-15 18:46:52 +0900

    WIP: nav.utils.ts 내 toAccordionItems의 onClick에서 경로 navigate 하기(#89)

--- src/components/Navigation/nav/nav.utils.ts
+++ src/components/Navigation/nav/nav.utils.ts
@@ -1,4 +1,5 @@
 // Acoordion용 아이템으로 변환
+import { PATHS } from '@/routes/constants'
 import type { NavItemDef } from './nav.types'
 import type { AccordionItem } from '@/components/Navigation/Accordion/Accordion.types'
 
@@ -12,6 +13,10 @@ export function toAccordionItems(
     activeIcon: d.activeIcon,
     label: d.label,
     active: active === d.key,
-    onClick: () => setActive(d.key),
+    onClick: () => {
+      setActive(d.key)
+      // TODO: 여기서 네비게이트!!!!
+      const segment = PATHS[d.key]
+    },
   }))
 }
