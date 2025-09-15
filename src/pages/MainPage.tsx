ee2bdc1d234462437b75f7c0604f4741c1f5d423
Makee <ina0312@naver.com>
2025-09-15 14:53:10 +0900

    feat: 메인 페이지 제작(#46)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,22 +1,34 @@
 import Navigaiton from '@/components/Navigation/Navigation'
+import { PAGE_TITLE } from '@/routes/constants'
 import { Suspense, useState } from 'react'
+import { Outlet, useLocation } from 'react-router'
+
+const getPageTitle = (pathname: string): string => {
+  const segment = pathname.split('/').filter(Boolean).pop() || ''
+  return PAGE_TITLE[segment.toUpperCase()] ?? ''
+}
 
 /** 메인 레이아웃 기능 수행 */
 export default function MainPage() {
+  const { pathname } = useLocation()
+
   const [drawerOpen, setDrawerOpen] = useState(false)
 
   return (
     <main className="flex">
       <aside className="relative h-screen flex-1 md:flex">
         <Navigaiton
-          actionbarTitle={NAV_TESTPAGE_TITLE}
+          actionbarTitle={getPageTitle(pathname)}
           mobileDrawerOpen={drawerOpen}
           onActionbarMenuClick={() => setDrawerOpen(true)}
           onMobileDrawerOpenChange={() => setDrawerOpen(false)}
         />
       </aside>
       <main className="flex-1 space-y-4 p-6">
-        <Suspense fallback={}></Suspense>
+        {/* TODO: BasicSkeleton으로 로딩 화면 대체 */}
+        <Suspense fallback={<div className="p-6">Loading…</div>}>
+          <Outlet key={pathname} />
+        </Suspense>
       </main>
     </main>
   )
