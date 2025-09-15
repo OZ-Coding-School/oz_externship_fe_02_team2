8017034bd1e816318c4132a891730bc1d99e0007
Makee <ina0312@naver.com>
2025-09-15 11:25:55 +0900

    feat: 라우트용 상수 파일 생성, 테스트 라우트용 기본 paths 추가(#46)

---
+++ src/routes/constants.ts
@@ -0,0 +1,17 @@
+export const TEST_PATHS = {
+  TEST: '/test-hub',
+  WEB: '/app',
+
+  LOGIN: '/login',
+
+  USER: '/user',
+  WITHDRAWAL: '/withdrawal',
+  DASHBOARD: '/dashboard',
+
+  LECTURE: '/lecture',
+  STUDYGROUP: '/studygroup',
+  REVIEW: '/review',
+
+  POST: '/post',
+  APPLICATION: '/application',
+}
