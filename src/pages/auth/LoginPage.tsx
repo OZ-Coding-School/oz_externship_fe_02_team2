737c2195acf83c27ef8f82edbfa2d28c494b14aa
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-09-17 16:07:37 +0900

    [Feat] 로그인 페이지 UI 제작 & 유효성 검사 (#97)
    * feat: TestRoot에 로그인 페이지 라우트 버튼 추가(#84)
    
    * feat: TestRoutes에 로그인 페이지 라우트 추가(#84)
    
    * chore: 페이지 심볼 에셋 추가(#84)
    
    * chore: 페이지 심볼 에셋 추가(#84)
    
    * feat: index.html title&심볼 적용(#84)
    
    * comment: 로그인 라우팅 관련 주석 추가(#84)
    
    * feat: LoginPage 큰 구조 잡기(#84)
    
    * feat: LoginPage md 이상 버전 작업(#84)
    
    * design: LoginPage md 미만 반응형 css 작업(#84)
    
    * refactor: 로그인 페이지 파츠 분리 및 연결(#84)
    
    * refactor: 로그인 Form 내 스타일 상수화(#84)
    
    * feat: 이메일&패스워드 유효성검사 유틸 추가(#84)
    
    * docs: 버튼 사용법 갱신- 버튼 타입 지정(#84)
    
    * feat: Button props에 type 명기, 기본값 button으로(#84)
    
    * feat: 유효성 검사 문구 수정(#84)
    
    * feat: Form 내 유효성 검사, 피드백 처리 로직 추가(#84)
    
    * design: 드래그 방지 위해 select-none 추가(#84)

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,7 +1,25 @@
+import { Description, Form, Header, RightYellowDiv } from './parts'
+
 export default function LoginPage() {
   return (
-    <main>
-      <h1>로그인 페이지</h1>
+    <main className="flex h-screen w-screen bg-white px-4 select-none md:px-0">
+      {/* 로그인 섹션: md 이상 너비 840px 고정 */}
+      <section className="flex h-full w-full items-center justify-center md:w-210 md:shrink-0">
+        {/* 로그인 영역: max-width 328px */}
+        <div className="flex w-82 flex-col items-center">
+          <Header />
+          <Description />
+          <Form />
+        </div>
+      </section>
+
+      <RightYellowDiv />
     </main>
   )
 }
+
+// 조합형 API
+LoginPage.Header = Header
+LoginPage.Description = Description
+LoginPage.Form = Form
+LoginPage.RightYellowDiv = RightYellowDiv
---
+++ src/pages/auth/parts/Description.tsx
@@ -0,0 +1,8 @@
+export default function Description() {
+  return (
+    <p className="body-xs mt-[46px] mb-6 leading-none tracking-tighter text-[#000A30]">
+      <span className="text-[#F6A818]">admin 계정</span>을 통해 로그인을
+      진행해주세요.
+    </p>
+  )
+}
