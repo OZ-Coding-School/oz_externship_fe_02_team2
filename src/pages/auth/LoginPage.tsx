a384b8b52a95730fd9ae232a4c2d3f4e42b44fec
Makee <ina0312@naver.com>
2025-09-17 11:43:54 +0900

    feat: LoginPage 큰 구조 잡기(#84)

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,7 +1,13 @@
 export default function LoginPage() {
   return (
-    <main>
-      <h1>로그인 페이지</h1>
+    <main className="flex h-screen w-screen bg-white">
+      {/* 로그인 섹션: md 이상 너비 840px 고정 */}
+      <section className="h-full md:w-210 md:shrink-0">
+        <div className="flex flex-col items-center"></div>
+      </section>
+
+      {/* 우측 노란색 디자인 섹션 (md 미만 hidden) */}
+      <div className="hidden bg-[#F6A818]/8 md:block md:flex-1"></div>
     </main>
   )
 }
