19d9901c46926cd1d8c98e2531c6a0d8e51e712c
Makee <ina0312@naver.com>
2025-09-18 19:00:18 +0900

    design: 공통 섹션 width 변경(#99)

--- src/pages/dashboard/sub-pages/Reason.tsx
+++ src/pages/dashboard/sub-pages/Reason.tsx
@@ -4,6 +4,7 @@ export default function Reason() {
   return (
     <>
       <Section title="탈퇴 사유 분포" />
+      <div className="h-6" />
       <Section title="탈퇴 사유별 월별 추세" />
     </>
   )
--- src/pages/dashboard/sub-pages/Section.tsx
+++ src/pages/dashboard/sub-pages/Section.tsx
@@ -4,13 +4,15 @@ type SectionProps = {
 
 export default function Section({ title }: SectionProps) {
   return (
-    <section className="flex w-[1120px] items-center justify-center rounded-lg bg-white p-6 shadow-xs">
-      <div className="flex w-[1072px] flex-col items-center gap-6">
+    <section className="flex min-w-[1120px] items-center justify-center rounded-lg bg-white p-6 shadow-xs">
+      <div className="flex w-full flex-col items-center gap-6">
         <header className="flex h-9 w-full items-center justify-between">
           <h5>{title}</h5>
           {/* 드롭다운?: 월별/연별 */}
         </header>
-        <article className="h-80 w-full">{/* 차트 들어올 곳 */}</article>
+        <article className="h-80 w-full">
+          {/* 차트 들어올 곳 (children으로 받을까...?) */}
+        </article>
       </div>
     </section>
   )
