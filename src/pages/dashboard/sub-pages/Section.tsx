caf340a7c3b8cbf59521510a1606198d98d06272
Makee <ina0312@naver.com>
2025-09-22 16:12:00 +0900

    feat: props에 children 추가 및 연결, 차트 받아올 목적(#99)

--- src/pages/dashboard/sub-pages/Section.tsx
+++ src/pages/dashboard/sub-pages/Section.tsx
@@ -1,12 +1,14 @@
 import Dropdown from '@/components/ui/Dropdown/Dropdown'
 import type { DropdownProps } from '@/components/ui/Dropdown/Dropdown.types'
+import type { ReactNode } from 'react'
 
 type SectionProps = {
   title: string
   dropdown?: DropdownProps
+  children: ReactNode
 }
 
-export default function Section({ title, dropdown }: SectionProps) {
+export default function Section({ title, dropdown, children }: SectionProps) {
   return (
     <section className="flex min-w-[1120px] items-center justify-center rounded-lg bg-white p-6 shadow-xs">
       <div className="flex w-full flex-col items-center gap-6">
@@ -14,9 +16,7 @@ export default function Section({ title, dropdown }: SectionProps) {
           <h5>{title}</h5>
           {dropdown && <Dropdown {...dropdown} />}
         </header>
-        <article className="h-80 w-full">
-          {/* 차트 들어올 곳 (children으로 받을까...?) */}
-        </article>
+        <article className="h-80 w-full">{children}</article>
       </div>
     </section>
   )
